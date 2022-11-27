90c8678eb84e6f6cbe5c6a517d535db614fa57ae
Marco Muser <marco.muser@posteo.de>
2022-11-27 11:32:16 +0100

    restructure code

--- minicron.mjs
+++ minicron.mjs
@@ -1,46 +1,66 @@
+// Utility functions
 const readLinesFromStdin = async (stream) => {
   const chunks = [];
   for await (const chunk of stream) chunks.push(chunk);
   return Buffer.concat(chunks).toString("utf8").split("\n");
 };
+
 const readTimeFromArgv = (argv) => argv.at(2).split(":");
+
 const logLine = (hours, minutes, day, task) =>
   console.log(`${hours}:${minutes} ${day} - ${task}`);
 
-const lines = await readLinesFromStdin(process.stdin);
-const [argHours, argMinutes] = readTimeFromArgv(process.argv);
+const isLineTimeGreaterEqual = (lineTime, argTime) =>
+  lineTime.getTime() >= argTime.getTime();
 
-for (const line of lines) {
-  const [minutes, hours, task] = line.split(" ");
+// Constants
+const today = "today";
+const tomorrow = "tomorrow";
 
-  if (![minutes, hours].includes("*")) {
-    const lineTime = new Date(2022, 1, 1, hours, minutes);
-    const argTime = new Date(2022, 1, 1, argHours, argMinutes);
+const main = async () => {
+  const lines = await readLinesFromStdin(process.stdin);
+  const [argHours, argMinutes] = readTimeFromArgv(process.argv);
 
-    if (lineTime.getTime() >= argTime.getTime()) {
-      logLine(hours, minutes, "today", task);
+  for (const line of lines) {
+    const [minutes, hours, task] = line.split(" ");
+
+    if (![minutes, hours].includes("*")) {
+      if (
+        isLineTimeGreaterEqual(
+          new Date(2022, 1, 1, hours, minutes),
+          new Date(2022, 1, 1, argHours, argMinutes)
+        )
+      ) {
+        logLine(hours, minutes, today, task);
+      } else {
+        logLine(hours, minutes, tomorrow, task);
+      }
+    } else if (hours === "*" && minutes !== "*") {
+      if (
+        isLineTimeGreaterEqual(
+          new Date(2022, 1, 1, 1, minutes),
+          new Date(2022, 1, 1, 1, argMinutes)
+        )
+      ) {
+        logLine(argHours, minutes, today, task);
+      } else {
+        logLine(Number(argHours) + 1, minutes, tomorrow, task);
+      }
+    } else if (hours !== "*" && minutes === "*") {
+      if (
+        isLineTimeGreaterEqual(
+          new Date(2022, 1, 1, hours, 1),
+          new Date(2022, 1, 1, argHours, 1)
+        )
+      ) {
+        logLine(hours, argMinutes, today, task);
+      } else {
+        logLine(Number(argHours) + 1, argMinutes, today, task);
+      }
     } else {
-      logLine(hours, minutes, "tomorrow", task);
+      logLine(argHours, argMinutes, today, task);
     }
-  } else if (hours === "*" && minutes !== "*") {
-    const lineTime = new Date(2022, 1, 1, 1, minutes);
-    const argTime = new Date(2022, 1, 1, 1, argMinutes);
-
-    if (lineTime.getTime() >= argTime.getTime()) {
-      logLine(argHours, minutes, "today", task);
-    } else {
-      logLine(Number(argHours) + 1, minutes, "tomorrow", task);
-    }
-  } else if (hours !== "*" && minutes === "*") {
-    const lineTime = new Date(2022, 1, 1, hours, 1);
-    const argTime = new Date(2022, 1, 1, argHours, 1);
-
-    if (lineTime.getTime() >= argTime.getTime()) {
-      logLine(hours, argMinutes, "today", task);
-    } else {
-      logLine(Number(argHours) + 1, argMinutes, "today", task);
-    }
-  } else {
-    logLine(argHours, argMinutes, "today", task);
   }
-}
+};
+
+await main();
