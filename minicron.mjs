e1fdd3fc65a850d5322abc9cf3e3973e277a01a9
Marco Muser <marco.muser@posteo.de>
2022-11-27 11:14:54 +0100

    add all other cases

--- minicron.mjs
+++ minicron.mjs
@@ -4,21 +4,43 @@ const readLinesFromStdin = async (stream) => {
   return Buffer.concat(chunks).toString("utf8").split("\n");
 };
 const readTimeFromArgv = (argv) => argv.at(2).split(":");
+const logLine = (hours, minutes, day, task) =>
+  console.log(`${hours}:${minutes} ${day} - ${task}`);
 
 const lines = await readLinesFromStdin(process.stdin);
 const [argHours, argMinutes] = readTimeFromArgv(process.argv);
 
 for (const line of lines) {
   const [minutes, hours, task] = line.split(" ");
-  const lineTime = new Date(2022, 1, 1, hours, minutes);
-  const argTime = new Date(2022, 1, 1, argHours, argMinutes);
 
   if (![minutes, hours].includes("*")) {
+    const lineTime = new Date(2022, 1, 1, hours, minutes);
+    const argTime = new Date(2022, 1, 1, argHours, argMinutes);
+
     if (lineTime.getTime() >= argTime.getTime()) {
-      console.log(`${hours}:${minutes} today - ${task}`);
+      logLine(hours, minutes, "today", task);
     } else {
-      console.log(`${hours}:${minutes} tomorrow - ${task}`);
+      logLine(hours, minutes, "tomorrow", task);
     }
-  } else if (hours === "*") {
+  } else if (hours === "*" && minutes !== "*") {
+    const lineTime = new Date(2022, 1, 1, 1, minutes);
+    const argTime = new Date(2022, 1, 1, 1, argMinutes);
+
+    if (lineTime.getTime() >= argTime.getTime()) {
+      logLine(argHours, minutes, "today", task);
+    } else {
+      logLine(Number(argHours) + 1, minutes, "tomorrow", task);
+    }
+  } else if (hours !== "*" && minutes === "*") {
+    const lineTime = new Date(2022, 1, 1, hours, 1);
+    const argTime = new Date(2022, 1, 1, argHours, 1);
+
+    if (lineTime.getTime() >= argTime.getTime()) {
+      logLine(hours, argMinutes, "today", task);
+    } else {
+      logLine(Number(argHours) + 1, argMinutes, "today", task);
+    }
+  } else {
+    logLine(argHours, argMinutes, "today", task);
   }
 }
